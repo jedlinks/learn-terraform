d2fe56d327b6d49663b7a89267935dddd54190e8
jedlinks <jedlinks@gmail.com>
2024-01-16 23:20:08 -0600

    Ansible server configurations

---
+++ expense-v1/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "frontend" {
+  ami                       = "ami-081609eef2e3cc958"
+  instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0012107af37e36eec"]
+
+  tags = {
+    Name = "frontend-dev"
+
+resource "aws_instance" "backend" {
+  ami                       = "ami-081609eef2e3cc958"
+  instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0012107af37e36eec"]
+
+  tags = {
+    Name = "backend-dev"
+
+resource "aws_instance" "mysql" {
+  ami                       = "ami-081609eef2e3cc958"
+  instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0012107af37e36eec"]
+
+  tags = {
+    Name = "mysql-dev"
