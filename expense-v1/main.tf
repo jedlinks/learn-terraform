e8f62e1f6af376dd07ddd261eafeed12b2c5bc37
jedlinks <jedlinks@gmail.com>
2024-01-17 17:19:50 -0600

    Ansible server configurations

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
     }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z05050322P8QFCN8M8LU9"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "backend" {
   ami                       = "ami-081609eef2e3cc958"
   instance_type             = "t3.micro"
@@ -18,6 +26,14 @@ resource "aws_instance" "backend" {
     }
 }
 
+resource "aws_route53_record" "backend" {
+  zone_id = "Z05050322P8QFCN8M8LU9"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami                       = "ami-081609eef2e3cc958"
   instance_type             = "t3.micro"
@@ -26,4 +42,12 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql-dev"
     }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z05050322P8QFCN8M8LU9"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
