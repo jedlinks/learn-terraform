a155be3073e354cdb6700a7cae7742727fa1de9c
jedlinks <jedlinks@gmail.com>
2024-01-17 19:00:56 -0600

    Ansible server configurations

---
+++ expense-v2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  default = "ami-081609eef2e3cc958"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0012107af37e36eec"]
+}
+
+variable "zone_id" {
+  default = "Z05050322P8QFCN8M8LU9"
+}
+
+variable "components" {
+   default = {"frontend, backend, mysql"}
+}
